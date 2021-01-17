a0b736d49a9511e7629f21478a91b31e192fb1f1
Rana Mostafa <ranamostafamohsen96@yahoo.com>
2021-01-17 20:39:03 +0200

    Prepared Misclassified bashscript for Croatian Dataset automatically

---
+++ Source_Code/MisClassification/misclassified_croatian.cmd
@@ -0,0 +1,8 @@
+copy ..\\DataSet\\Croatian_DataSet\\rmastif_Testing\\00001\\00002_00000.ppm MisClassification_Croatian_Images\00002_00000_3_1.ppm
+copy ..\\DataSet\\Croatian_DataSet\\rmastif_Testing\\00001\\00007_00000.ppm MisClassification_Croatian_Images\00007_00000_2_1.ppm
+copy ..\\DataSet\\Croatian_DataSet\\rmastif_Testing\\00004\\00002_00003.ppm MisClassification_Croatian_Images\00002_00003_8_4.ppm
+copy ..\\DataSet\\Croatian_DataSet\\rmastif_Testing\\00010\\00018_00000.ppm MisClassification_Croatian_Images\00018_00000_0_10.ppm
+copy ..\\DataSet\\Croatian_DataSet\\rmastif_Testing\\00011\\00011_00002.ppm MisClassification_Croatian_Images\00011_00002_7_11.ppm
+copy ..\\DataSet\\Croatian_DataSet\\rmastif_Testing\\00011\\00011_00003.ppm MisClassification_Croatian_Images\00011_00003_5_11.ppm
+copy ..\\DataSet\\Croatian_DataSet\\rmastif_Testing\\00012\\00009_00003.ppm MisClassification_Croatian_Images\00009_00003_6_12.ppm
+copy ..\\DataSet\\Croatian_DataSet\\rmastif_Testing\\00019\\00004_00000.ppm MisClassification_Croatian_Images\00004_00000_20_19.ppm
